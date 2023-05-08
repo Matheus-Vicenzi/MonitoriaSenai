0c11788de4a48edbbf9d3084e26c61b2016918a0
Matheus Vicenzi <matheus.vicenzi2000@gmail.com>
2023-05-08 20:48:41 -0300

    removida pasta 'front'

--- src/components/Recover/Recover.jsx
+++ src/components/Recover/Recover.jsx
@@ -6,30 +6,34 @@ import MailRoundedIcon from "@mui/icons-material/MailRounded";
 import Inputs from "../Inputs/Inputs";
 import "./Recover.css";
 export default function Recover() {
-  return (
-    <>
-      <Link to="/">
-        <ArrowBackIosIcon className="voltar" />
-      </Link>
+	return (
+		<>
+			<Link to="/">
+				<ArrowBackIosIcon className="voltar" />
+			</Link>
 
-      <LogoSenai url="/src/assets/imgs/logo-unisenai.png" alt="Logo UniSenai" />
+			<LogoSenai
+				url="/src/assets/imgs/logo-unisenai.png"
+				alt="Logo UniSenai"
+			/>
 
-      <div className="login-box">
-        <h1>Recuperar Senha</h1>
+			<div className="login-box">
+				<h1>Recuperar Senha</h1>
 
-        <div
-          className="field-box"
-          style={{
-            width: "100%",
-          }}>
-          <Inputs name="Seu E-mail" icon={<MailRoundedIcon />} />
-          <button className="button-fieldR">RECUPERAR</button>
+				<div
+					className="field-box"
+					style={{
+						width: "100%",
+					}}
+				>
+					<Inputs name="Seu E-mail" icon={<MailRoundedIcon />} />
+					<button className="button-fieldR">RECUPERAR</button>
 
-          <p className="registre">
-            <Link to="/RecoverPassword"> Pass</Link>
-          </p>
-        </div>
-      </div>
-    </>
-  );
+					<p className="registre">
+						<Link to="/RecoverPassword"> Pass</Link>
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
